d7e33bb6725bbce44da031d2df349b9b5938553f
DVSwitch <38995311+DVSwitch@users.noreply.github.com>
2021-12-20 06:28:22 -0500

    change date

--- scripts/AMBEtest4_p3.py
+++ scripts/AMBEtest4_p3.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #
-#   Copyright (C) 2017 Mike Zingman N4IRR
+#   Copyright (C) 2021 Mike Zingman N4IRR
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
